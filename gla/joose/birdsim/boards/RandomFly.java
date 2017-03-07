080dd4bb2aaabbaf3dd0a56eae378b3cebf7f530
sosaje <jonathansaje@glaroam2-131-33.wireless.gla.ac.uk>
2017-03-07 17:01:21 +0000

    Added instances

--- gla/joose/birdsim/boards/RandomFly.java
+++ gla/joose/birdsim/boards/RandomFly.java
@@ -1,17 +1,21 @@
 package gla.joose.birdsim.boards;
 
 import gla.joose.birdsim.pieces.Bird;
+import java.util.Random;
+
 
 /**
  * Created by jonathansaje on 07/03/2017.
  */
 public class RandomFly implements FlyBehaviour {
 
-    public void fly() {
+    Random rand = new Random();
+
+    public void fly(Board board) {
 
         Bird bird = new Bird();
 
-        int randRow = Board.rand.nextInt((Board.getRows() - 3) + 1) + 0;
+        int randRow = board.rand.nextInt((board.getRows() - 3) + 1) + 0;
         int randCol = rand.nextInt((getColumns() - 3) + 1) + 0;
 
         place(bird,randRow, randCol);
